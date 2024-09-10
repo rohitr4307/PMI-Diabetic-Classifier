b52c1458f6131f514f4c9f8af6ae2b0e45d49ce1
Rohit Ranjan <53111621+rohitr4307@users.noreply.github.com>
2024-09-10 16:26:06 +0530

    upadted path

--- Prediction.py
+++ Prediction.py
@@ -1,5 +1,5 @@
 import joblib
 
 def predict(data):
-    clf = joblib.load("best_model.joblib")
+    clf = joblib.load("/workspaces/PMI-Diabetic-Classifier/best_model.joblib")
     return clf.predict(data)
