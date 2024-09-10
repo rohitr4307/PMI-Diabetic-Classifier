8ddf5524999aaede02db4358c9e857310890417b
Rohit Ranjan <53111621+rohitr4307@users.noreply.github.com>
2024-09-10 16:45:57 +0530

    Update Prediction.py

--- Prediction.py
+++ Prediction.py
@@ -1,5 +1,5 @@
 import joblib
 
 def predict(data):
-    clf = joblib.load("/pmi-diabetic-classifier/master/output/best_model_v1.joblib")
+    clf = joblib.load("/home/runner/work/PMI-Diabetic-Classifier/PMI-Diabetic-Classifier/best_model.joblib")
     return clf.predict(data)
