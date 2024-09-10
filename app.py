4a8c11bf8bacf845b74cdc06e7c43232a18a705c
Rohit Ranjan <53111621+rohitr4307@users.noreply.github.com>
2024-09-10 12:28:00 +0530

    Added markdown and changed header to title

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
 import streamlit as st
 
-st.header("PIMA Diabetic Classification")
+st.title("PIMA Diabetic Classification")
+st.markdown(
+    '''
+    This dataset is originally from the National Institute of Diabetes and Digestive and Kidney Diseases. 
+    The objective of the dataset is to diagnostically predict whether or not a patient has diabetes, 
+    based on certain diagnostic measurements included in the dataset. Several constraints were placed on the
+    selection of these instances from a larger database.
+    In particular, all patients here are females at least 21 years old of Pima Indian heritage.
+    '''
+)
 
 col1, col2 = st.columns(2)
 
